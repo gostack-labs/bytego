9dfdcf2872cdbf292951a3515a5bc30f3074063f
alovn <alovn@live.com>
2022-04-11 00:10:10 +0800

    refactor: remove http2

--- app.go
+++ app.go
@@ -4,15 +4,11 @@ import (
 	"context"
 	"net"
 	"net/http"
-
-	"golang.org/x/net/http2"
-	"golang.org/x/net/http2/h2c"
 )
 
 type App struct {
-	server   *http.Server
-	UseHTTP2 bool
-	route    *route
+	server *http.Server
+	route  *route
 	Router
 }
 
@@ -32,11 +28,7 @@ func (a *App) Use(middlewares ...HandlerFunc) {
 }
 
 func (a *App) Handler() http.Handler {
-	if !a.UseHTTP2 {
-		return a
-	}
-	h2s := &http2.Server{}
-	return h2c.NewHandler(a, h2s)
+	return a
 }
 
 func (a *App) Run(addr string) error {
--- go.mod
+++ go.mod
@@ -1,7 +1,3 @@
 module github.com/gostack-labs/bytego
 
 go 1.17
-
-require golang.org/x/net v0.0.0-20220407224826-aac1ed45d8e3
-
-require golang.org/x/text v0.3.7 // indirect
